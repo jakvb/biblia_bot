c6930828a12705d20054b71dba068b57caacd54b
jakub <jakvbko@gmail.com>
2021-04-20 22:47:53 +0200

    prefix

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from txt_formating import Book
 log = logging.getLogger()
 logging.basicConfig(level=logging.DEBUG, filename=os.path.dirname(os.path.abspath(__file__))+'/app.log')
 
-bot = commands.Bot(command_prefix= "@")
+bot = commands.Bot(command_prefix= "$")
 AUDIO_PATH = os.path.dirname(os.path.abspath(__file__)) + '/audio/'
 FILENAME_TEMPLATE = '{}_{}'
 state = {}
